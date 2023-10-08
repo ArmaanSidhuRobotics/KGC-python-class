0c41d64bfe850b5b115d439d8ae5e5538f616a5d
Armaan Sidhu <armaansidhu000@icloud.com>
2023-10-08 12:11:25 -0700

    KGC Code 10-8-2023

--- kgcCode.py
+++ kgcCode.py
@@ -1,6 +1,13 @@
 print("Hello, welcome to my game!")
 x = input("What is your name?: ")
 print("Hello " + x + ", welcome to my game ! Good luck!")
+print("lets play " + x + "!")
+
+print(x * 4)
+print(2 * 4)
+print(true * 2)
+d = 2
+
 
 
 
---
+++ testPython.py
@@ -0,0 +1,18 @@
+name = input("What is your name: ")
+favRace = input("What is your favroite race: ")
+print(name + "'s favorite race is "+favRace)
+favRace = "white people"
+print(name + "'s Acutal favorite race is "+favRace)
+
+i = 5
+d = i * 2.0 
+d /= 3
+print(d)
+
+arabOne = "hussain osama"
+arabTwo = "Laden al-hussien "
+
+print(arabOne + " " +  arabTwo)
+
+print(i > d)
+
